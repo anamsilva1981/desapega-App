e1fc2262c2719321cca9b61d84c81daa3b1c716d
anamsilva1981 <ana.msilva@hotmail.com>
2025-11-20 16:23:00 -0300

    feat(menu): adiciona componente menu com navega;'ao e estilos

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
